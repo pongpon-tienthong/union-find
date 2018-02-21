312309fefa0805a3769f4b7bdd95b95f1f7ef33d
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2018-02-21 08:10:47 -0600

    Implement Quick Union

---
+++ src/com/pongpon/unionfind/QuickUnionUF.java
@@ -0,0 +1,28 @@
+package com.pongpon.unionfind;
+
+public class QuickUnionUF {
+
+    private int[] id;
+
+    public QuickUnionUF(int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+
+    private int root(int i) {
+        while(i != id[i]) i = id[i];
+        return i;
+    }
+
+    public boolean connected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    public void union(int p, int q) {
+        int pRoot = root(p);
+        int qRoot = root(q);
+        id[pRoot] = qRoot;
+    }
+}
